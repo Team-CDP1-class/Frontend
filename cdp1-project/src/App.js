328083f97aad10a28ce1cf5c72cb75fbba3e1bd5
Kwon Da Woon <momnpa333@gmail.com>
2023-10-06 17:18:48 +0900

    Feat: make-frame

--- cdp1-project/src/App.js
+++ cdp1-project/src/App.js
@@ -1,7 +1,35 @@
+import { Route, Routes, Outlet } from "react-router-dom";
+import MainPage from "./components/views/MainPage/MainPage";
+import LoginPage from "./components/views/LoginPage/LoginPage";
+import RegisterPage from "./components/views/RegisterPage/RegisterPage";
+import Navbar from "./components/views/NavBar/NavBar";
+import Footer from "./components/views/Footer/Footer";
+
 import "./App.css";
 
+function Layout() {
+    return (
+        <div>
+            <Navbar />
+            <main>
+                <Outlet />
+            </main>
+            <Footer />
+        </div>
+    );
+}
+
 function App() {
-    return <div className="App">hello world</div>;
+    return (
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<MainPage />} />
+
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
