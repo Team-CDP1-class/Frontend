a0d917bff4112697eb70315a5cbc0b84f4e642f6
JYN523 <87629686+JYN523@users.noreply.github.com>
2023-10-16 06:17:49 +0900

    add element design & ElementPage

--- src/layout/Navbar/index.jsx
+++ src/layout/Navbar/index.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className='navbar'>
+      <button className='navbarMenu float-left text-myColors-primary font-bold'>LOGO</button>
+      <button className='navbarMenu float-left'>스토리 카드</button>
+      <button className='navbarMenu float-left'>스토리 전개 분석</button>
+      <button className='navbarMenu float-right'>회원가입/로그인</button>
+    </div>
+  );
 };
 
 export default Navbar;
