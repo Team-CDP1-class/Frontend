ed35d45100c354c739b25f3d5de5a10a07714a57
Kwon Da Woon <momnpa333@gmail.com>
2023-10-18 16:34:22 +0900

    Fix:login page

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -24,14 +24,15 @@ const LoginPage = () => {
   const userPassword = {
     required: "필수 필드입니다.",
   };
+
   return (
     <section className="flex flex-col justify-center mt-20 max-w-[400px] m-auto">
-      <div className="p-6 bg-sky-500/20 rounded-md shadow-md">
+      <div className="mt-20 border p-6 rounded-md shadow-md">
         <h1 className="text-3xl font-semibold text-center">로그인</h1>
         <form className="mt-6" onSubmit={handleSubmit(onSubmit)}>
           <div className="mb-2">
             <label htmlFor="email" className="text-sm font-semibold text-gray-800">
-              Email
+              이메일
             </label>
             <input
               type="email"
@@ -48,7 +49,7 @@ const LoginPage = () => {
 
           <div className="mb-2">
             <label htmlFor="password" className="text-sm font-semibold text-gray-800">
-              Password
+              비밀번호
             </label>
             <input
               type="password"
@@ -66,14 +67,14 @@ const LoginPage = () => {
           <div className="mt-6">
             <button
               type="submit"
-              className="w-full px-4 py-2 text-white duration-200 bg-black rounded-md hover:bg-gray-700"
+              className="w-full px-4 py-2 text-white duration-200 bg-myColors-primary  rounded-md hover:bg-myColors-primary/80"
             >
               로그인
             </button>
           </div>
 
           <p className="mt-8 text-xs font-light text-center text-gray-700">
-            아이디가 없다면?{" "}
+            아직 회원이 아니신가요?{" "}
             <a href="/register" className="font-medium hover:underline">
               회원가입
             </a>
