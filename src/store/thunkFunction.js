604e4a9f30e1b68ce3e2d60143fc3af6f500e12e
Kwon Da Woon <momnpa333@gmail.com>
2023-11-02 17:20:49 +0900

    Feat:add thunkfunciont

--- src/store/thunkFunction.js
+++ src/store/thunkFunction.js
@@ -11,3 +11,13 @@ export const registerUser = createAsyncThunk("user/registerUser", async (body, t
     return thunkAPI.rejectWithValue(error.response.data || error.message);
   }
 });
+
+export const loginUser = createAsyncThunk("user/loginUser", async (body, thunkAPI) => {
+  try {
+    const response = await axiosInstance.post(`/users/login`, body);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    return thunkAPI.rejectWithValue(error.response.data || error.message);
+  }
+});
